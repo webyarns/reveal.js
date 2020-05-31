da8db7bf1545c39d2ea62dbb67679db8750dcb26
Raphael Parree <rparree@edc4it.com>
2020-05-31 16:30:41 +0200

    added auto-video-play to this project

---
+++ webyarns-util/lib/auto-video-play.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+(function (factory) {
+  if ((typeof exports === "undefined" ? "undefined" : _typeof(exports)) === 'object') {
+    // Node. Does not work with strict CommonJS.
+    module.exports = factory();
+  } else {
+    // Browser globals.
+    Object.assign(window, factory());
+    window.addEventListener("DOMContentLoaded", function () {
+      Reveal.addEventListener('ready', window.autoplayVideo);
+      Reveal.addEventListener('slidechanged', window.autoplayVideo);
+    });
+  }
+})(function () {
+  function autoplayVideo(event) {
+    event.currentSlide.querySelectorAll("video[data-autostart]").forEach(function (e) {
+      if (!(e instanceof HTMLVideoElement)) {
+        return;
+      } // noinspection JSIgnoredPromiseFromCall
+
+
+      e.play(); // e.addEventListener("ended",)
+    });
+    if (event.previousSlide) event.previousSlide.querySelectorAll("video[data-autostart]").forEach(function (e) {
+      if (!(e instanceof HTMLVideoElement)) {
+        return;
+      } // noinspection JSIgnoredPromiseFromCall
+
+
+      e.pause(); // e.removeEventListener("ended",proceedToNext)
+    });
+  }
+
+  return {
+    autoplayVideo: autoplayVideo
+  };
+});
