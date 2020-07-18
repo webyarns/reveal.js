4e42c37ffda817ad3e5ba24fa19587287151c42d
Raphael Parree <rparree@edc4it.com>
2020-07-18 11:18:13 +0200

    deprecated audio-ios-initialiser.ts and auto-video-play.ts

--- webyarns-util/src/audio-ios-initialiser.ts
+++ webyarns-util/src/audio-ios-initialiser.ts
@@ -1,16 +1,1 @@
-(function () {
-    function preloadAllAudio(evt: Event) {
-        evt.preventDefault()
-        document.querySelectorAll("audio").forEach((elem) => {
-            elem.play().then(()=>elem.pause())
-        })
-        Reveal.next();
-    }
-
-    document.addEventListener("DOMContentLoaded", () => {
-        const audioInitLink = document.getElementById("ipad-audio-init");
-        if (!audioInitLink)
-            return
-        ['touchstart', 'click'].forEach(name =>  audioInitLink.addEventListener(name,preloadAllAudio) )
-    })
-})()
+console.log("Deprecated script: audio-ios-initialiser. Is no longer needed")
