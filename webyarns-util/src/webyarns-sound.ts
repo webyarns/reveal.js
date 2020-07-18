6f9073ed41d7bc42788f3f864be52524215f113d
Raphael Parree <rparree@edc4it.com>
2020-07-18 13:04:25 +0200

    [sounds] Added ability to start audio and stop on another slide.

--- webyarns-util/src/webyarns-sound.ts
+++ webyarns-util/src/webyarns-sound.ts
@@ -8,7 +8,7 @@
     }
 }(() => {
     type AudioId = string
-    const audioId = (s: AudioId): AudioId => s.startsWith("!") ? s.substring(1) : s;
+    const audioId = (s: AudioId): AudioId => (s.startsWith("!") || s.startsWith(">") || s.startsWith("#")) ? s.substring(1) : s;
 
     const partition = <T>(es: Array<T>, fn: (a: T) => boolean) =>
         es.reduce(([p, f], e) => (fn(e) ? [[...p, e], f] : [p, [...f, e]]), [[], []]);
@@ -53,18 +53,28 @@
     const soundData = (s: string | null) =>
         s ? s.split(",").map(s => s.trim()) : [];
 
-
+    let persistentAudios: string[] = []
     const nextAudioActions = (currentSounds: string[], nextSounds: string[]): [string[], string[]] => {
-        const [toRestart, nextToStartIds] = partition(nextSounds, e => e.startsWith("!"));
+        const [toRestart, nextToStartIds]: [string[], string[]] = partition(nextSounds, e => e.startsWith("!"));
+
+        const persistentToStop = nextToStartIds.filter(e => e.startsWith("#")).map(audioId);
+        const persistentStart = nextToStartIds.filter(e => e.startsWith(">")).map(audioId);
+        persistentAudios = [...persistentAudios, ...persistentStart] // add to global state
+
         const currentSoundIds = currentSounds.map(audioId);
         const nextSoundsIds = nextSounds.map(audioId);
         const toRestartIds = toRestart.map(audioId);
-        const toStop = currentSoundIds
-            .filter(e => !nextToStartIds.includes(e)) // remove the ones that carry over
-            .filter(e => !toRestartIds.includes(e)) // remove the ones that need restarting
+        const toStop = [
+            ...currentSoundIds
+                .filter(e => !nextToStartIds.includes(e)) // remove the ones that carry over
+                .filter(e => !toRestartIds.includes(e)) // remove the ones that need restarting
+                .filter(e => persistentAudios.indexOf(e) === -1),
+            ...persistentToStop]
         const toStart = [
-            ...nextSoundsIds.filter(e => !currentSoundIds.includes(e)), // add new ones not carried over
-            ...toRestartIds // add the ones that need restarted
+            ...nextSoundsIds
+                .filter(e => !currentSoundIds.includes(e)) // add new ones not carried over
+                .filter(e => !persistentToStop.includes(e)), // remove the ones intended to stop
+            ...toRestartIds, // add the ones that need restarted
         ]
         return [toStop, toStart]
     }
@@ -103,8 +113,8 @@
 
 
     }
-    Reveal.addEventListener('ready', soundHandler);
-    Reveal.addEventListener('slidechanged', soundHandler);
+    // Reveal.addEventListener('ready', soundHandler);
+    // Reveal.addEventListener('slidechanged', soundHandler);
     return {
         soundHandler,
         _test: {
