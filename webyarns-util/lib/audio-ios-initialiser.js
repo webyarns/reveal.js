513763151f6d9c182f1ff06d7bfb1da971240a8d
Raphael Parree <rparree@edc4it.com>
2020-07-18 13:19:08 +0200

    forgot to checking built deprecated video scripts

--- webyarns-util/lib/audio-ios-initialiser.js
+++ webyarns-util/lib/audio-ios-initialiser.js
@@ -1,21 +1,3 @@
 "use strict";
 
-(function () {
-  function preloadAllAudio(evt) {
-    evt.preventDefault();
-    document.querySelectorAll("audio").forEach(function (elem) {
-      elem.play().then(function () {
-        return elem.pause();
-      });
-    });
-    Reveal.next();
-  }
-
-  document.addEventListener("DOMContentLoaded", function () {
-    var audioInitLink = document.getElementById("ipad-audio-init");
-    if (!audioInitLink) return;
-    ['touchstart', 'click'].forEach(function (name) {
-      return audioInitLink.addEventListener(name, preloadAllAudio);
-    });
-  });
-})();
+console.log("Deprecated script: audio-ios-initialiser. Is no longer needed");
