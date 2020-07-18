4e11cd973811f720f25be629487a11b1e9d3bb65
Raphael Parree <rparree@edc4it.com>
2020-07-18 11:18:13 +0200

    re-added auto-video-play.ts with deprecation warning

--- webyarns-util/src/auto-video-play.ts
+++ webyarns-util/src/auto-video-play.ts
@@ -1,1 +1,42 @@
-console.log("Deprecated script: auto-video-play.ts. Use reveal's built-in support")
+
+(function (factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS.
+        module.exports = factory();
+    } else {
+        // Browser globals.
+        Object.assign(window,factory())
+        window.addEventListener("DOMContentLoaded", ()=>{
+            Reveal.addEventListener('ready', window.autoplayVideo)
+            Reveal.addEventListener('slidechanged', window.autoplayVideo)
+        })
+    }
+}(() => {
+    console.log("Deprecated script: auto-video-play.ts. Use reveal's built-in support https://revealjs.com/media/#autoplay  (data-autoplay)")
+    function autoplayVideo(event: SlideEvent) {
+        event.currentSlide.querySelectorAll("video[data-autostart]").forEach(e => {
+            if (!(e instanceof HTMLVideoElement)) {
+                return;
+            }
+            // noinspection JSIgnoredPromiseFromCall
+            e.play()
+            // e.addEventListener("ended",)
+        })
+        if (event.previousSlide)
+            event.previousSlide.querySelectorAll("video[data-autostart]").forEach(e => {
+                if (!(e instanceof HTMLVideoElement)) {
+                    return;
+                }
+                // noinspection JSIgnoredPromiseFromCall
+                e.pause()
+                // e.removeEventListener("ended",proceedToNext)
+            })
+
+
+    }
+
+
+    return {
+        autoplayVideo,
+    }
+}));
