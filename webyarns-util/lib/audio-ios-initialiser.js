fcf025808e6a004fb54bb8baadf141fb732c6c71
Raphael Parree <rparree@edc4it.com>
2020-09-13 09:33:01 +0200

    re-added audio-ios-initialiser with deprecation warning

--- webyarns-util/lib/audio-ios-initialiser.js
+++ webyarns-util/lib/audio-ios-initialiser.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log("Deprecated script: audio-ios-initialiser. Is no longer needed");
+(function () {
+  function preloadAllAudio(evt) {
+    evt.preventDefault();
+    document.querySelectorAll("audio").forEach(function (elem) {
+      elem.play().then(function () {
+        return elem.pause();
+      });
+    });
+    Reveal.next();
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("Deprecated script: audio-ios-initialiser. Is no longer needed");
+    var audioInitLink = document.getElementById("ipad-audio-init");
+    if (!audioInitLink) return;
+    ['touchstart', 'click'].forEach(function (name) {
+      return audioInitLink.addEventListener(name, preloadAllAudio);
+    });
+  });
+})();
--- webyarns-util/src/audio-ios-initialiser.ts
+++ webyarns-util/src/audio-ios-initialiser.ts
@@ -1,1 +1,19 @@
-console.log("Deprecated script: audio-ios-initialiser. Is no longer needed")
+(function () {
+    function preloadAllAudio(evt: Event) {
+        evt.preventDefault()
+        document.querySelectorAll("audio").forEach((elem) => {
+            elem.play().then(()=>elem.pause())
+        })
+        Reveal.next();
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+        console.log("Deprecated script: audio-ios-initialiser. Is no longer needed")
+
+        const audioInitLink = document.getElementById("ipad-audio-init");
+        if (!audioInitLink)
+            return
+        ['touchstart', 'click'].forEach(name =>  audioInitLink.addEventListener(name,preloadAllAudio) )
+    })
+})()
+
