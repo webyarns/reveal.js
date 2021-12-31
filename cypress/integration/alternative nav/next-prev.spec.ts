87e33f5e25fca6fac9c408edd00644619783acf5
Raphael Parree <rparree@edc4it.com>
2021-12-31 12:46:50 +0100

    feat: added swipe to test for next-prev-spec

--- cypress/integration/alternative nav/next-prev.spec.ts
+++ cypress/integration/alternative nav/next-prev.spec.ts
@@ -52,9 +52,22 @@ describe('combined data-previous-slide-indexh and data-next-slide-indexh', () =>
     cy.previousSlide()
     cy.currentSlide().should('have.id',"slide-7")
   })
+
   it('user should be stuck when going left', () => {
     cy.visit('webyarns-prev-next-slides.html#/slide-7')
     cy.nextSlide()
+    cy.currentSlide().should('have.id',"slide-7")
+  })
+
+  it('user should be stuck swiping left', () => {
+    cy.visit('webyarns-prev-next-slides.html#/slide-7')
+    cy.swipeToLeft()
+    cy.currentSlide().should('have.id',"slide-7")
+  })
+
+  it('user should be stuck swiping right', () => {
+    cy.visit('webyarns-prev-next-slides.html#/slide-7')
+    cy.swipeToRight()
     cy.currentSlide().should('have.id',"slide-7")
   })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -6,9 +6,20 @@ declare namespace Cypress {
         currentSlide(): Chainable<Subject>
         nextSlide(): Chainable<Subject>
         previousSlide(): Chainable<Subject>
+        swipeToLeft(): Chainable<Subject>
+        swipeToRight(): Chainable<Subject>
     }
 }
 
 Cypress.Commands.add('currentSlide', () => cy.get("section.present",{timeout:500}))
 Cypress.Commands.add('nextSlide', () => cy.get("body").type("{rightArrow}"))
 Cypress.Commands.add('previousSlide', () => cy.get("body").type("{leftArrow}"))
+
+Cypress.Commands.add('swipeToLeft', () => cy.get('body')
+    .trigger('pointerdown', { which: 1 })
+    .trigger('pointermove', 'right')
+    .trigger('pointerup', { force: true }))
+Cypress.Commands.add('swipeToRight', () => cy.get('body')
+    .trigger('pointerdown', { which: 1 })
+    .trigger('pointermove', 'left')
+    .trigger('pointerup', { force: true }))
