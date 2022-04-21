4e2640507b5c979bb104b149700bb91ed3ff700f
Raphael Parree <rparree@edc4it.com>
2022-04-21 13:02:18 +0200

    feat(test): added demo and test for (non-)data-touch-only-section

---
+++ cypress/integration/hidden sections/touch-only-desktop.spec.ts
@@ -0,0 +1,24 @@
+describe('data-touch-only-section', () => {
+
+  it('Should be hidden on non-touch', () => {
+    cy.visit('webyarns-touch-dependent.html#/s0')
+    cy.nextSlide()
+    cy.currentSlide().should('have.id',"s2")
+  })
+
+
+})
+
+
+
+describe('data-non-touch-only-section', () => {
+
+  it('Should be visible on non-touch', () => {
+    cy.visit('webyarns-touch-dependent.html#/s2')
+    cy.nextSlide()
+    cy.currentSlide().should('have.id',"s3")
+  })
+
+
+})
+
