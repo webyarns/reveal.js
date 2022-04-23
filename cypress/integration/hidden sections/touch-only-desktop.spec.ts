6b7e0dfb12c35325c65d79bbc36638e9209bec32
Raphael Parree <rparree@edc4it.com>
2022-04-23 09:59:32 +0200

    feat(test): added test for autoslide with (non-)data-touch-only-section

--- cypress/integration/hidden sections/touch-only-desktop.spec.ts
+++ cypress/integration/hidden sections/touch-only-desktop.spec.ts
@@ -1,23 +1,28 @@
 describe('data-touch-only-section', () => {
 
-  it('Should be hidden on non-touch', () => {
-    cy.visit('webyarns-touch-dependent.html#/s0')
-    cy.nextSlide()
-    cy.currentSlide().should('have.id',"s2")
-  })
+    it('Should be hidden on non-touch', () => {
+        cy.visit('webyarns-touch-dependent.html#/s0')
+        cy.nextSlide()
+        cy.currentSlide().should('have.id', "s2")
+    })
 
+    it('Should be skipped from data-autoslide', () => {
+        cy.clock();
+        cy.visit('webyarns-touch-dependent.html#/s5')
+        cy.tick(200)
+        cy.currentSlide().should('have.id', "s7")
+    })
 
 })
 
 
-
 describe('data-non-touch-only-section', () => {
 
-  it('Should be visible on non-touch', () => {
-    cy.visit('webyarns-touch-dependent.html#/s2')
-    cy.nextSlide()
-    cy.currentSlide().should('have.id',"s3")
-  })
+    it('Should be visible on non-touch', () => {
+        cy.visit('webyarns-touch-dependent.html#/s2')
+        cy.nextSlide()
+        cy.currentSlide().should('have.id', "s3")
+    })
 
 
 })
